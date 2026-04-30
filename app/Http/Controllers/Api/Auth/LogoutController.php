8880a6914aef60ab48cd7a6bb1396cfa3046fc78
Sebastian Soczka <sebastian.soczka@gmail.com>
2026-04-30 10:07:34 +0200

    fix: add PHPStan null guard for user in LogoutController

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -10,7 +11,9 @@ class LogoutController extends Controller
 {
     public function __invoke(Request $request): Response
     {
-        $request->user()->currentAccessToken()->delete();
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
         return response()->noContent();
     }
